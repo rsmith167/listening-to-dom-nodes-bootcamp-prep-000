fdb8e81a324f522b8d55a5b58dde942bad7a4fa7
Rwanda S <rwanda.ace@gmail.com>
2020-01-28 20:25:13 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const myMain = document.getElementById('main');
+ 
+myMain.addEventListener('click', function(event) {
+  alert('I was clicked!'); 
+  });
+  
